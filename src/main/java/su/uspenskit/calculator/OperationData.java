c67e77355f6e5b60788638c44453a91696f48675
wellWINeo <usa200313@gmail.com>
2022-11-18 10:46:52 +0300

    add pr1

---
+++ src/main/java/su/uspenskit/calculator/OperationData.java
@@ -0,0 +1,31 @@
+package su.uspenskit.calculator;
+
+public class OperationData {
+    private double A;
+    private double B;
+    private char Op;
+
+    public double getA() {
+        return A;
+    }
+
+    public void setA(double a) {
+        A = a;
+    }
+
+    public double getB() {
+        return B;
+    }
+
+    public void setB(double b) {
+        B = b;
+    }
+
+    public char getOp() {
+        return Op;
+    }
+
+    public void setOp(char op) {
+        Op = op;
+    }
+}
---
+++ src/test/java/su/uspenskit/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package su.uspenskit.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
